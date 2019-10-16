12fe7ae111514e18b0630dc77a8948db38e6ef56
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-10-16 15:23:22 -0400

    files updated/cleaned

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -5,36 +5,25 @@ import axios from 'axios';
 
 const Movie = (props) => {
 
-
   const [movie, setMovie] = useState({});
 
   useEffect(() => {
       
-    const id = 1;
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
+    const id = props.match.params.id;
 
     axios
-         
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
         })
-         
         .catch(error => {
           console.error(error);
         });
+  },[id]);
 
-  },[]);
-  
-  // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
 
   if (!movie) {
-    return <div>Loading movie information...</div>;
+    return <div>Loading Movie Information...</div>;
   }
 
   const { title, director, metascore, stars } = movie;
@@ -42,7 +31,7 @@ const Movie = (props) => {
   return (
 
     <div className="save-wrapper">
-
+      
       <div className="movie-card">
 
         <h2>{title}</h2>
@@ -50,7 +39,7 @@ const Movie = (props) => {
         <div className="movie-director">
           Director: <em>{director}</em>
         </div>
-        
+
         <div className="movie-metascore">
           Metascore: <strong>{metascore}</strong>
         </div>
@@ -58,14 +47,12 @@ const Movie = (props) => {
         <h3>Actors</h3>
 
         {stars.map(star => (
-
           <div key={star} className="movie-star">
             {star}
           </div>
-
         ))}
       </div>
-
+      
       <div className="save-button">Save</div>
 
     </div>
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 
 const SavedList = props => (
   <div className="saved-list">
@@ -6,8 +9,10 @@ const SavedList = props => (
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div className="home-button">Home</div>
+    <Link to="/" className="home-button">Home</Link>
   </div>
 );
+
+
 
 export default SavedList;
